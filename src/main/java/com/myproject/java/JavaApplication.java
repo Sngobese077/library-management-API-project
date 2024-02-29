d405caa3bdbe137b892d286465b00090245e4aa5
Sngobese077 <91742887+Sngobese077@users.noreply.github.com>
2024-02-29 16:42:03 +0200

    Update JavaApplication.java

--- src/main/java/com/myproject/java/JavaApplication.java
+++ src/main/java/com/myproject/java/JavaApplication.java
@@ -1,1 +1,20 @@
+package com.myproject.java;
 
+import com.myproject.java.entity.Books;
+import com.myproject.java.entity.Student;
+import com.myproject.java.service.BooksServiceDAO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+}
