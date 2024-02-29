828b2e50e46ea48af457cab5387ac3869eef643f
Sngobese077 <91742887+Sngobese077@users.noreply.github.com>
2024-02-29 16:52:43 +0200

    Update BooksServiceDAO.java

--- src/main/java/com/myproject/java/service/BooksServiceDAO.java
+++ src/main/java/com/myproject/java/service/BooksServiceDAO.java
@@ -1,1 +1,24 @@
+package com.myproject.java.service;
 
+import com.myproject.java.entity.Books;
+import com.myproject.java.entity.Student;
+
+import java.util.List;
+
+public interface BooksServiceDAO
+{
+    //FOR BOOKS
+    List<Books> findAllBooks();
+    Books save_books(Books theBooks);
+    Books findById_Books(int theId_books);
+    void deleteById_Book(int theBooks);
+    void update_Book(Books theBooks);
+
+    //FOR STUDENT
+    List<Student> findAllStudent();
+    Student save_students(Student theStudents);
+    Student findById_Student(int theId_student);
+    void deleteById_Student(int theStudent);
+    void update_student(Student theStudent);
+
+}
