3ce8d371bffaf57a4373d2e16c8b67fc6348394a
Sngobese077 <91742887+Sngobese077@users.noreply.github.com>
2024-02-29 16:43:21 +0200

    Update LibraryDAO.java

--- src/main/java/com/myproject/dao/LibraryDAO.java
+++ src/main/java/com/myproject/dao/LibraryDAO.java
@@ -1,1 +1,23 @@
+package com.myproject.java.dao;
 
+import com.myproject.java.entity.Books;
+import com.myproject.java.entity.Student;
+
+import java.util.List;
+
+public interface LibraryDAO
+{
+    //FOR BOOKS
+    List<Books> findAllBooks();
+    Books save(Books theBooks);
+    Books findById_Books(int theId_books);
+    void deleteById_Book(int theBooks);
+    void update(Books theBooks);
+
+    //FOR STUDENT
+    List<Student> findAllStudent();
+    Student save(Student theStudents);
+    Student findById_Student(int theId_student);
+    void deleteById_Student(int theStudent);
+    void update(Student theStudent);
+}
