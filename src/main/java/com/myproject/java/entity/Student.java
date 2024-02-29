1beca6a3b0dfa22916aa7fadce4b508b9b7bac82
Sngobese077 <91742887+Sngobese077@users.noreply.github.com>
2024-02-29 16:55:09 +0200

    Update Student.java

--- src/main/java/com/myproject/java/entity/Student.java
+++ src/main/java/com/myproject/java/entity/Student.java
@@ -1,1 +1,47 @@
+package com.myproject.java.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int reg_no;
+
+    @Column(name = "student_name")
+    private String student_name;
+
+    public Student() {
+    }
+
+    public Student(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public int getReg_no() {
+        return reg_no;
+    }
+
+    public void setReg_no(int reg_no) {
+        this.reg_no = reg_no;
+    }
+
+    public String getStudent_name() {
+        return student_name;
+    }
+
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "reg_no=" + reg_no +
+                ", student_name='" + student_name + '\'' +
+                '}';
+    }
+}
