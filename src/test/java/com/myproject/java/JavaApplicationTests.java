88fdb892ca130440d84100766af39ccd544096a2
Sngobese077 <91742887+Sngobese077@users.noreply.github.com>
2024-02-29 16:59:36 +0200

    Update JavaApplicationTests.java

--- src/test/java/com/myproject/java/JavaApplicationTests.java
+++ src/test/java/com/myproject/java/JavaApplicationTests.java
@@ -1,1 +1,13 @@
+package com.myproject.java;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
